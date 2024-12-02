8c4ca04fd59f7cc4b76c79960383d7bc56a67718
José Ferreira <josedsferreira@gmail.com>
2024-12-02 18:16:15 +0000

    Implement user authentication with User, UserRepository, AuthorizationService, and UserRole classes

---
+++ src/main/java/dev/jf/starFraction/auth/users/UserRole.java
@@ -0,0 +1,22 @@
+package dev.jf.starFraction.auth.users;
+
+public enum UserRole {
+
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
