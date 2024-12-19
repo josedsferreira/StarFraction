be2f538a8a421c5de9b8e2a7c0b9759c4d5c17f8
José Ferreira <josedsferreira@gmail.com>
2024-12-19 16:53:40 +0000

    Lombok added

--- src/main/java/dev/jf/starFraction/planets/Planet.java
+++ src/main/java/dev/jf/starFraction/planets/Planet.java
@@ -8,8 +8,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "planet")
 public class Planet {
 
@@ -23,32 +25,6 @@ public class Planet {
 
     @Enumerated(EnumType.STRING)
     @Column(name="planet_size")
-    private PlanetSize planetSize;
-
-    public Long getPlanetId() {
-        return planetId;
-    }
-
-    public void setPlanetId(Long planetId) {
-        this.planetId = planetId;
-    }
-
-    public String getPlanetName() {
-        return planetName;
-    }
-
-    public void setPlanetName(String planetName) {
-        this.planetName = planetName;
-    }
-
-    public PlanetSize getPlanetSize() {
-        return planetSize;
-    }
-
-    public void setPlanetSize(PlanetSize planetSize) {
-        this.planetSize = planetSize;
-    }
-
-    
+    private PlanetSize planetSize;    
 
 }
